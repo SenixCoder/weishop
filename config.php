234b5b35b079ce7c737ae8bd26ec1899612265f2
Mike Tang <mike@mikecoder.net>
2015-07-06 23:25:02 +0800

    weishop css fixed

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'title'=>array(//配置在表单中的键名 ,这个会是config[random]
-		'title'=>'商城名称:',//表单的文字
-		'type'=>'text',		 //表单的类型：text、textarea、checkbox、radio、select等
-		'value'=>'Mike的微商城',			 //表单的默认值
-	),
-);
-					
